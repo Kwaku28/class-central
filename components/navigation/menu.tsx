0807eb3360f8652b10853594194aa253f5b42771
Kwaku28 <rekenburg777@gmail.com>
2025-07-11 10:49:33 +0000

    Update classNames and icon path

--- components/navigation/menu.tsx
+++ components/navigation/menu.tsx
@@ -1,4 +1,4 @@
-// import { currentUser } from "@clerk/nextjs/server";
+import { currentUser } from "@clerk/nextjs/server";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -118,28 +118,28 @@ const menuItems = [
 ];
 
 const Menu = async () => {
-  // const user = await currentUser();
-  // const role = user?.publicMetadata.role as string;
+  const user = await currentUser();
+  const role = user?.publicMetadata.role as string;
   return (
-    <div className="mt-4 text-sm overflow-y-scroll h-[calc(100vh-4rem)] remove-scrollbar">
+    <div className="mt-4 text-sm">
       {menuItems.map((i) => (
         <div className="flex flex-col gap-2" key={i.title}>
           <span className="hidden lg:block text-gray-400 font-light my-4">
             {i.title}
           </span>
           {i.items.map((item) => {
-            // if (item.visible.includes(role)) {
+            if (item.visible.includes(role)) {
               return (
                 <Link
                   href={item.href}
                   key={item.label}
-                  className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 rounded-md hover:bg-lamaSkyLight"
+                  className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 rounded-md hover:bg-classSkyLight"
                 >
-                  <Image src={item.icon} alt="menu item" width={20} height={20} />
+                  <Image src={item.icon} alt="menu icon" width={20} height={20} />
                   <span className="hidden lg:block">{item.label}</span>
                 </Link>
               );
-            // }
+            }
           })}
         </div>
       ))}
