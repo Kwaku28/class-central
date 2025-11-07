fdb81e277261e2bd974bbcbaf36aae76e5d12caf
Kwaku28 <rekenburg777@gmail.com>
2025-11-07 22:04:39 +0000

    Add validations for lesson form

--- lib/formValidationSchemas.ts
+++ lib/formValidationSchemas.ts
@@ -123,3 +123,18 @@ export const messageSchema = z.object({
 });
 
 export type MessageSchema = z.infer<typeof messageSchema>;
+
+export const lessonSchema = z.object({
+  id: z.coerce.number().optional(),
+  subjectId: z.coerce.number().min(1, { message: "Subject is required!" }),
+  classId: z.coerce.number().min(1, { message: "Class is required!" }),
+  teacherId: z.string().min(1, { message: "Teacher is required!" }),
+  day: z.enum(
+    ["MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY", "SUNDAY"],
+    { message: "Day is required!" }
+  ),
+  startTime: z.coerce.date({ message: "Start time is required!" }),
+  endTime: z.coerce.date({ message: "End time is required!" }),
+});
+
+export type LessonSchema = z.infer<typeof lessonSchema>;
