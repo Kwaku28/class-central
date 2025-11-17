197b2892ce8e375de9942f704b98803943e362c4
Kwaku28 <rekenburg777@gmail.com>
2025-11-17 01:58:34 +0000

    Add filter options to page
    Implemented the search, sort and filter functionalities.

--- app/(dashboard)/list/teachers/page.tsx
+++ app/(dashboard)/list/teachers/page.tsx
@@ -1,16 +1,15 @@
 import prisma from "@/lib/prisma";
 import { Class, Prisma, Subject, Teacher } from "@prisma/client";
-
 import Image from "next/image";
 import Link from "next/link";
-
 import { auth } from "@clerk/nextjs/server";
-
 import { ITEM_PER_PAGE } from "@/lib/settings";
 import FormContainer from "@/components/list/FormContainer";
 import TableSearch from "@/components/list/TableSearch";
 import Table from "@/components/list/Table";
 import Pagination from "@/components/list/Pagination";
+import { getTeacherColumns } from "@/lib/data";
+import SortFilterControls from "@/components/list/SortFilterControls";
 
 type TeacherList = Teacher & { subjects: Subject[] } & { classes: Class[] };
 
@@ -21,45 +20,8 @@ const TeacherListPage = async ({
 }) => {
   const { sessionClaims } = await auth();
   const role = (sessionClaims?.metadata as { role?: string })?.role;
-  const columns = [
-    {
-      header: "Info",
-      accessor: "info",
-    },
-    {
-      header: "Teacher ID",
-      accessor: "teacherId",
-      className: "hidden md:table-cell",
-    },
-    {
-      header: "Subjects",
-      accessor: "subjects",
-      className: "hidden md:table-cell",
-    },
-    {
-      header: "Classes",
-      accessor: "classes",
-      className: "hidden md:table-cell",
-    },
-    {
-      header: "Phone",
-      accessor: "phone",
-      className: "hidden lg:table-cell",
-    },
-    {
-      header: "Address",
-      accessor: "address",
-      className: "hidden lg:table-cell",
-    },
-    ...(role === "admin"
-      ? [
-          {
-            header: "Actions",
-            accessor: "action",
-          },
-        ]
-      : []),
-  ];
+
+  const columns = getTeacherColumns(role);
 
   const renderRow = (item: TeacherList) => (
     <tr
@@ -69,7 +31,7 @@ const TeacherListPage = async ({
       <td className="flex items-center gap-4 p-4">
         <Image
           src={item.img || "/icons/noAvatar.png"}
-          alt=""
+          alt="Avatar"
           width={40}
           height={40}
           className="md:hidden xl:block w-10 h-10 rounded-full object-cover"
@@ -104,33 +66,17 @@ const TeacherListPage = async ({
   );
 
   const params = await searchParams;
-  const { page, ...queryParams } = params;
+  const { page, search, sortBy, sortDir } = params;
 
   const p = page ? parseInt(page) : 1;
 
-  // URL PARAMS CONDITION
-
   const query: Prisma.TeacherWhereInput = {};
-
-  if (queryParams) {
-    for (const [key, value] of Object.entries(queryParams)) {
-      if (value !== undefined) {
-        switch (key) {
-          case "classId":
-            query.lessons = {
-              some: {
-                classId: parseInt(value),
-              },
-            };
-            break;
-          case "search":
-            query.name = { contains: value, mode: "insensitive" };
-            break;
-          default:
-            break;
-        }
-      }
-    }
+  if (search) {
+    query.OR = [
+      { name: { contains: search, mode: "insensitive" } },
+      { surname: { contains: search, mode: "insensitive" } },
+      { username: { contains: search, mode: "insensitive" } },
+    ];
   }
 
   const [data, count] = await prisma.$transaction([
@@ -142,6 +88,10 @@ const TeacherListPage = async ({
       },
       take: ITEM_PER_PAGE,
       skip: ITEM_PER_PAGE * (p - 1),
+      orderBy:
+        sortBy && sortDir
+          ? { [sortBy]: sortDir as "asc" | "desc" }
+          : { name: "asc" },
     }),
     prisma.teacher.count({ where: query }),
   ]);
@@ -154,12 +104,13 @@ const TeacherListPage = async ({
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <TableSearch />
           <div className="flex items-center gap-4 self-end">
-            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-class-yellow cursor-pointer">
-              <Image src="/icons/filter.png" alt="" width={14} height={14} />
-            </button>
-            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-classYellow cursor-pointer">
-              <Image src="/icons/sort.png" alt="" width={14} height={14} />
-            </button>
+            <SortFilterControls
+              sortOptions={[
+                { label: "Name", value: "name" },
+                { label: "Surname", value: "surname" },
+              ]}
+              defaultSort={{ sortBy: "name", sortDir: "asc" }}
+            />
             {role === "admin" && (
               <FormContainer table="teacher" type="create" />
             )}
